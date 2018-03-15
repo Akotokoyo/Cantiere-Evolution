b504770e3c0c1b1e2283c952c2ec676d22b984ab
Alessio@Tedde <alessiocena@hotmail.it>
2018-03-15 15:32:44 +0100

    Introdotta Scena iniziale, non è però inquadrato bene su telefono

---
+++ Assets/Script/TitleSceneScript/CreditScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditScript : MonoBehaviour {
+
+    public GameObject creditpanel;
+
+    public void ShowCredit()//Mostra il pannello dei crediti
+    {
+        creditpanel.transform.position= new Vector3(360, 540, 0);
+    }
+    public void HideCredit()//Nasconde il pannello dei crediti
+    {
+        creditpanel.transform.position = new Vector3(-750, 0, 0);
+    }
+}
---
+++ Assets/Script/TitleSceneScript/LanguageScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageScript : MonoBehaviour {
+
+    public Image english;
+    public Image italian;
+    public Text Testo;
+
+    public void Start()
+    {
+        italian.color = new Color(255, 255, 255, 1);
+        english.color = new Color(255, 255, 255, 0.5f);
+        Testo.text = "Tap to Play";
+    }
+
+    public void onclick(bool lan)
+        //I Bottoni con le lingue, sono settati in modo che se lan(language) è:
+        //TRUE La lingua settata è l'INGLESE
+        //FALSE La lingua settata è l'ITALIANO
+        {
+            if (lan)
+            {
+                english.color = new Color(255,255,255,0.5f);
+                italian.color = new Color(255, 255, 255, 1);
+                Testo.text = "Tap to Play";
+        }
+        else
+            {
+                italian.color = new Color(255, 255, 255, 0.5f);
+                english.color = new Color(255, 255, 255, 1);
+                Testo.text = "Premi Per Giocare";
+        }
+    }
+}
