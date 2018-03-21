e8596eadd64383da827832195a88931f9cdc8348
Alessio@Tedde <alessiocena@hotmail.it>
2018-03-21 22:06:06 +0100

    messa l'animazione dei cantieri se cliccati, l'animazione parte ogni 6 secondi(se ovviamente cliccato), e la lettura è di 5 secondi, spotbutton ora spunta se ci sono 2 o meno vecchietti e sparisce altrimenti

--- Assets/Script/GameScript/ManagerScript.cs
+++ Assets/Script/GameScript/ManagerScript.cs
@@ -10,12 +10,12 @@ public class ManagerScript : MonoBehaviour {
     public float spawnWait;
     public float startWait;
     private bool oldmantype;
+    public GameObject spotButton;
 
     public int oldManLimit;
     private int manCont;
 
     //GESTIONE SCORE
-    public float nexttouch;
     public Text finalscore;
     private int attualscore;
 
@@ -24,6 +24,7 @@ public class ManagerScript : MonoBehaviour {
 
     //VARIABILI INERENTI AL TOUCH
     RaycastHit hit;
+    public float nextTouch;
 
     //VARIABILE DI LINGUAGGIO
     private string lang;
@@ -31,6 +32,11 @@ public class ManagerScript : MonoBehaviour {
     string[] italianWorkerDialogue = { "Ma non vai a ritirare la pensione?", "I piccioni hanno fame..", "Torna in guerra al posto di rompere a noi", "Ma non avete nipoti da guardare?", "Vai a disturbare gli operai statali", "Vuoi venire a farlo te?", "Ma aspettare la morte come gli altri no?", "Ai tuoi tempi eravate tutti abusivi", "Senti, vai al bar, te lo pago io", "Oh ma te ne vai?" };
     string[] englishOldManDialogue = { "Stupid Generation", "There's no religion anymore", "They were the Gypsies", "This job isn't good", "You must bring respect to those who have made war" };
     string[] italianOldManDialogue = { "Generazione incapace", "Non c'è più religione", "Sono stati gli Zinghiri", "Non si fa mica così quel lavoro lì", "Bisogna portare rispetto a chi ha fatto la guerra"};
+    public GameObject workerDialogueButton;
+    public Text workerDialogueText;
+    public GameObject oldManDialogueButton;
+    public Text oldManDialogueText;
+
 
     void Start()
     {
@@ -54,52 +60,82 @@ public class ManagerScript : MonoBehaviour {
 
         StartCoroutine(SpawnWaves());
 
-        //LANGUAGE FROM ANOTHER SCRIPT
-        lang = PlayerPrefs.GetString("Language");
+        //LANGUAGE FROM ANOTHER SCRIPT, English or Italian
+        //lang = PlayerPrefs.GetString("Language");     COMMENTO SOLO PER PROVARE INGLESE
+        lang = "English";
     }
 
     IEnumerator SpawnWaves()
     {
         yield return new WaitForSeconds(startWait);
-        if(manCont < 10)
+        if(true)
         {
-                for (int i = 0; i < oldManLimit; i++)
-                {
-                    Vector3 spawnPosition = new Vector3(Random.Range(-1.5f, 1.5f), Random.Range(-3, 0), -1);
-                    Quaternion spawnRotation = Quaternion.identity;
-                    
-                if (oldmantype) {
-                    Debug.Log("uno");
-                    Instantiate(oldman1, spawnPosition, spawnRotation);
-                    oldmantype = false;
-                }
-                else
-                {
-                    Debug.Log("due");
-                    Instantiate(oldman2, spawnPosition, spawnRotation);
-                    oldmantype = true;
-                }
-                yield return new WaitForSeconds(spawnWait);
-                }
+            for (int i = 0; i < oldManLimit; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-1.5f, 1.5f), Random.Range(-3, 0), -1);
+                Quaternion spawnRotation = Quaternion.identity;
+
+            //What type of oldman will spawn
+            if (oldmantype) {
+//                Debug.Log("uno");
+                Instantiate(oldman1, spawnPosition, spawnRotation);
+                oldmantype = false;
+                manCont++;
+            }
+            else if(!oldmantype)
+            {
+//                Debug.Log("due");
+                Instantiate(oldman2, spawnPosition, spawnRotation);
+                oldmantype = true;
+                manCont++;
+            }
+
+            //Show and hide Spot Button
+            if (manCont <= 2)
+            {
+                spotButton.gameObject.SetActive(true);
+            }
+            else if(manCont > 2)
+            {
+                spotButton.gameObject.SetActive(false);
+            }
+            yield return new WaitForSeconds(spawnWait);
+            }
         }
     }
 
+    IEnumerator ShowMessageWorker(float delay)
+    {
+        int rand = Random.Range(0,10);
+        workerDialogueButton.gameObject.SetActive(true);
+        if (lang == "English") { 
+            workerDialogueText.text = englishWorkerDialogue[rand];
+        }
+        else
+        {
+            workerDialogueText.text = italianWorkerDialogue[rand];
+        }
+        yield return new WaitForSeconds(delay);
+        workerDialogueButton.gameObject.SetActive(false);
+    }
 
-    //GESTIONE DEL TOUCH
+
     void Update()
     {
+    //GESTIONE DEL TOUCH
         int i = 0;
         while (i < Input.touchCount)
         {
-            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            if (Input.GetTouch(i).phase == TouchPhase.Began && Time.time > nextTouch)
             {
+                nextTouch = Time.time + 6.0f;
                 Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(i).position);
                 RaycastHit hit = new RaycastHit();
                 if (Physics.Raycast(ray.origin, ray.direction, out hit)) {
-                    if(hit.transform.tag=="Worker") { 
-                        Debug.Log("Worker Touch");
-                        finalscore.text = "" + attualscore;
-                        attualscore++;
+                    if(hit.transform.tag=="Worker") {
+                        StartCoroutine(ShowMessageWorker(5.5f));
+//                        finalscore.text = "" + attualscore;
+//                        attualscore++;
                     }
                     else
                     {
